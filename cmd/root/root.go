6b66fb9d489c2d4c8e18a9789df338219b6e57a9
skitalex <drizzleent@yandex.ru>
2024-02-03 01:16:06 +0300

    add cobra

---
+++ cmd/root/root.go
@@ -0,0 +1,23 @@
+package root
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "chat",
+	Short: "cli-chat",
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+
+}
